5d7cd8bf9326cbb2ed2e3993eca5513fa9e53aa9
Lee McAlilly <leemcalilly@gmail.com>
2020-04-30 12:46:01 -0500

    Add fonts to option 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,40 @@
 module.exports = {
-  purge: [],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: [
+        'Raleway',
+        'system-ui',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        'Roboto',
+        '"Helvetica Neue"',
+        'Arial',
+        '"Noto Sans"',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+        '"Noto Color Emoji"',
+      ],
+      display: [
+        'Raleway',
+        'system-ui',
+        '-apple-system',
+        'Roboto',
+        'sans-serif'
+      ],
+      body: [
+      'Josefin Slab',
+      'Georgia',
+      'Cambria',
+      'serif'
+      ]
+    },
+
+    extend: {
+    }
   },
-  variants: {},
-  plugins: [],
+  variants: {
+  }
 }
